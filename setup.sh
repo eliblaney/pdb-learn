47aa07f88085665558712a65973073a5cfa676b1
Eli Blaney <eliblaney@gmail.com>
2021-03-19 10:46:09 -0500

    Add pdbbind link to setup script

--- setup.sh
+++ setup.sh
@@ -8,4 +8,4 @@ curl https://stringdb-static.org/download/protein.aliases.v11.0/9606.protein.ali
 gunzip stringsdb-v11.0.nosync/*
 
 echo "Note: The STRING file headers are occasionally not formatted properly. If there are any octothorpes ('#' symbols) on the first line, then please remove them."
-echo "Note: You will also need to download the PDBbind refined and general sets."
+echo "Note: You will also need to download the PDBbind refined and general sets at http://pdbbind.org.cn/download.php"
