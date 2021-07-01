19de5c79f0f09ae465ec720840ea4e14e93b3389
Eli Blaney <eliblaney@gmail.com>
2021-07-01 11:36:25 -0500

    Chunk training

--- train.py
+++ train.py
@@ -1,6 +1,8 @@
+import os
 import config
 import strings
 import logging
+import joblib
 import numpy as np
 from models.BayesianRidgeRegression import BayesianRidgeRegression
 from models.LogisticRegression import LogisticRegression
@@ -21,6 +23,45 @@ default_mutators = [
     PermutationalMutator,
     GeneticMutator,
 ]
+
+def train_partitioned(sdb, folder='data', models=default_models, mutators=default_mutators):
+    inputs = [file for file in os.listdir(folder) if 'input' in file]
+    outputs = [x.replace('input', 'output') for x in inputs]
+
+    for mt in default_models:
+        mutator = PermutationalMutator(mt)
+        while mutator.has_next():
+            m = mutator.next()
+            logging.debug("[%s] Created %s with options: %s", mutator, m, mutator.get_current_options())
+            m.save()
+
+    models = [file for file in os.listdir('saved_models')]
+    len_models = len(models)
+
+    total_partitions = len(inputs)
+    i = 0
+    with alive_bar(total_partitions, 'Partition') as bar:
+        while i < total_partititions:
+            x = np.load(folder + '/' + inputs[i])
+            y = np.load(folder + '/' + outputs[i])
+            
+            with alive_bar(len_models, 'Models') as bar2:
+                for mp in models:
+                    m = load_model(mp)
+
+                    logging.debug("Training %s on partition %s", m.get_full_name(), str(i))
+                    try:
+                        m.fit(x, y)
+                    except ValueError:
+                        logging.warning("Only one class found, skipping...")
+                    except Exception as e:
+                        logging.error("Unexpected exception: %s", e)
+
+                    logging.debug("Accuracy of %s: %s", m.get_full_name(), m.get_accuracy())
+                    m.save()
+                    bar2()
+            bar()
+            i = i + 1
 
 def train(sdb, models=default_models, mutators=default_mutators, inputs_file='inputs.np', outputs_file='outputs.np'):
     """Train models on given PDB data"""
@@ -50,9 +91,30 @@ def train(sdb, models=default_models, mutators=default_mutators, inputs_file='in
                     m.save()
                     bar()
 
+def load_model(path):
+    if not os.path.exists(path):
+        return None
+
+    name = os.path.basename(path).split(".")[0]
+    model = joblib.load(path)
+    m = None
+    if name == 'Bayesian Ridge Regression':
+        m = BayesianRidgeRegression(None)
+    elif name == 'Logistic Regression':
+        m = LogisticRegression(None)
+    elif name == 'Random Forest Classifier':
+        m = RandomForestClassifier(None)
+    elif name == 'Support Vector Classifier':
+        m = SupportVectorClassifier(None)
+    else:
+        return None
+
+    m.model = model
+    return m
+
 if __name__ == "__main__":
     logging.info("Importing databases...")
     sdb = strings.StringsDB()
     logging.info("Finished importing databases.")
 
-    train(sdb)
+    train_partitioned(sdb)
