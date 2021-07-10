581b8dd7a2a6f6a29e0c7af1a68956ec40d1b535
Eli Blaney <eliblaney@gmail.com>
2021-07-10 10:23:59 -0500

    Optimize memory

--- pdb.py
+++ pdb.py
@@ -1,4 +1,3 @@
-import hashlib
 import numpy as np
 import warnings
 from Bio.PDB.PDBExceptions import PDBConstructionWarning
@@ -13,7 +12,7 @@ with warnings.catch_warnings():
         """Get a representation of the PDB data of a particular protein"""
 
         rs = build_residues(structure_id, pdb_filename, [])
-        rs = build_residues(structure_id, pocket_filename, rs)
+        # rs = build_residues(structure_id, pocket_filename, rs)
 
         # TODO: Add ligand primary sequences
 
@@ -24,23 +23,29 @@ with warnings.catch_warnings():
 
     def build_residues(structure_id, filename, rs):
         s = parser.get_structure(structure_id, filename)
+        residues = []
+        atoms = []
         for m in s:
             for c in m:
                 for r in c:
-                    # Encodes a residue ("ILE") as a number (23471766)
-                    r_hashid = int(
-                        hashlib.sha1(
-                            r.get_resname().encode('UTF-8')).hexdigest(),
-                        16) % (10**8)
-                    residue = [r_hashid]
+                    # Encodes a residue ("ILE") as a number
+                    r_id = r.get_resname()
+                    if r_id in residues:
+                        r_id = residues.index(r_id)
+                    else:
+                        residues.append(r_id)
+                        r_id = residues.index(r_id)
+                    residue = [r_id]
                     # The following adds each atom with its coordinates and b-factor
                     for a in r:
                         # Encodes the atom name as a number
-                        a_hashid = int(
-                            hashlib.sha1(
-                                a.get_id().encode('UTF-8')).hexdigest(),
-                            16) % (10**8)
-                        residue.append(a_hashid)
+                        a_id = a.get_id()
+                        if a_id in atoms:
+                            a_id = atoms.index(a_id)
+                        else:
+                            atoms.append(a_id)
+                            a_id = atoms.index(a_id)
+                        residue.append(a_id)
                         residue.append(a.get_bfactor())
                         np.concatenate([residue, a.get_coord()])
                     # Add the array of residue hash + atom hash, b factor, coordinates for each atom and add
